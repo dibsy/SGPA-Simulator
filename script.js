5a23071bb6ecd4d0b55700a0d6a6485d687f674c
Dibyendu <dibyendu.coder@gmail.com>
2014-01-26 11:31:03 +0530

    Updated to v2.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// SGPA Calculator 1.0 Developer: Dibyendu (inDi-hex-k0d3r)
+// SGPA Calculator v2.0 Developer: Dibyendu (dibsyhex)
 var tcredits=0;// total credits of the all the subjects
 var scredits=0;// total credits secured by the student
 var arr=new Array();
@@ -26,7 +26,8 @@ var getGradePoint=function(grade){
 
 
 var creditsBox=function(){
-	for(i=1;i<=7;i++)
+	var count=document.getElementById("count").value;
+	for(i=1;i<=count;i++)
 	{
 		c="c"+parseInt(i);
 		g="g"+parseInt(i);
@@ -46,7 +47,8 @@ var creditsBox=function(){
 
 var calcSGPA=function(){
 	tcredits=creditsBox();
-	for(i=1;i<=7;i++)
+	var count=document.getElementById("count").value;
+	for(i=1;i<=count;i++)
 	{
 		
 		scredits+=arr[i];
@@ -54,4 +56,25 @@ var calcSGPA=function(){
 	sgpa=parseFloat(scredits/tcredits);
 	msg="SGPA : "+sgpa;
 	alert(msg);
+	location.reload();
+};
+
+/* The function below generates input DOM based on the input provided */
+var generate=function(){
+	//var text="Hello";
+	//alert(text);
+	var form=document.getElementById("formcontent");
+	var count=document.getElementById("count").value;
+	//alert(count);
+	var i;
+	var string='<b>Enter Credit and Select Grade<b><br><br>';
+	for(i=1;i<=count;i++)
+	{
+		string+=('<input class="input-small" id="c'+(i)+'" type="text">&nbsp;&nbsp;<select id="g'+(i)+'"><option value="O">O</option value="E"><option>E</option><option value="A">A</option><option value="B">B</option><option value="C">C</option><option value="D">D</option><option value="F">F</option></select><br><br>');
+	}
+	string+='<button type="button" class="btn btn-danger btn-sm" onclick="calcSGPA();">Calculate Result</button>';
+	form.innerHTML=string;
+
+	
+
 };
